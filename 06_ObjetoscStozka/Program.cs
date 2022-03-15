2b89a970335cbc38db8ea2bb2ef78ffb1f6f5854
Krystian Petek <krystianpetek2@gmail.com>
2022-03-15 14:22:00 +0100

    obj

---
+++ 06_ObjetoscStozka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _06_ObjetoscStozka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wejscie = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            int.TryParse(wejscie[0], out int promien);
+            int.TryParse(wejscie[1], out int tworzaca);
+
+        }
+    }
+}
