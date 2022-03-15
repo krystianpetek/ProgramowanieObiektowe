fbd99f73e55540abdcd47de0c71a678c1568ae9a
Krystian Petek <krystianpetek2@gmail.com>
2022-03-15 13:22:40 +0100

    update

--- 05_RownanieKwadratowe/Program.cs
+++ 05_RownanieKwadratowe/Program.cs
@@ -6,38 +6,68 @@ namespace _05_RownanieKwadratowe
     {
         static void Main(string[] args)
         {
-            QuadraticEquation(0, 0, 1);
+            QuadraticEquation(0, 2, 1);
+            QuadraticEquation(1000000, 6000, -300295);
         }
         public static void QuadraticEquation(int a, int b, int c)
         {
-            
-            if (a == 0 && b == 0 && c == 0)
+            long A = a;
+            long B = b;
+            long C = c;
+
+            if (A == 0 && B == 0 && C == 0)
             {
                 Console.WriteLine("infinity");
                 return;
             }
-            if (a == 0)
+
+            if (A == 0 && B == 0)
             {
                 Console.WriteLine("empty");
                 return;
             }
-            double delta = (b * b) - (4 * a * c);
+            if (B == 0 || C == 0)
+            {
+                Console.WriteLine($"x=0.00");
+                return;
+            }
+
+            long delta = (B * B) - (4 * A * C);
             if (delta > 0)
             {
-                double x1 = (-b - Math.Sqrt(delta)) / 2 * a;
-                double x2 = (-b + Math.Sqrt(delta)) / 2 * a;
-                Console.WriteLine($"x1={Math.Round(x1,2):F2}");
-                Console.WriteLine($"x2={Math.Round(x2,2):F2}");
+                if (A == 0)
+                {
+                    double x = Math.Round(C * -1.0 / B,2);
+                    Console.WriteLine($"x={x:F2}");
+                }
+                else
+                {
+                    //double x1 = (-B - Math.Sqrt(delta)) / 2 * A;
+                    //double x2 = (-B + Math.Sqrt(delta)) / 2 * A;
+                    double x1 = Math.Round(((B * -1) + Math.Sqrt(delta)) / (2 * A), 2);
+                    double x2 = Math.Round((((B * -1) + (Math.Sqrt(delta) * -1)) / (2 * A)), 2);
+                    if(x1 > x2)
+                    {
+                        double temp = x1;
+                        x1 = x2;
+                        x2 = temp;
+                    }
+                    Console.WriteLine($"x1={Math.Round(x1, 2):F2}");
+                    Console.WriteLine($"x2={Math.Round(x2, 2):F2}");
+                    return;
+                }
             }
             else if (delta == 0)
             {
-                double x0 = -b / (2 * a);
+                double x0 = -B / (2 * A);
 
-                Console.WriteLine($"x={Math.Round(x0,2):F2}");
+                Console.WriteLine($"x={Math.Round(x0, 2):F2}");
+                return;
             }
             else
             {
                 Console.WriteLine("empty");
+                return;
             }
         }
     }
