a424bd64c9b2b5e0ee4f44f09f3762ed84b4e9bd
Krystian Petek <krystianpetek2@gmail.com>
2022-03-17 21:00:10 +0100

    Update Program.cs

--- 12_PrzetwarzanieTablic1D_2/Program.cs
+++ 12_PrzetwarzanieTablic1D_2/Program.cs
@@ -9,14 +9,51 @@ namespace _12_PrzetwarzanieTablic1D_2
             int[] a = new int[] { -2, -1, 0, 1, 4 };
             int[] b = new int[] { -3, -2, -1, 1, 2, 3 };
             
-            a = new int[] { -2, -1, 0, 1, 4 };
+            a = new int[] { -2, -1, 0, 1, 1, 4 };
             b = new int[] { -3, -2, -1, 1, 2, 3 };
-            
+
+            a = new int[] { 1 };
+            b = new int[] { 2 };
             Print(a, b);
         }
         public static void Print(int[] a, int[] b)
         {
+            string wyjscie = string.Empty;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i] == b[j])
+                    {
+                        wyjscie += $"{a[i]} ";
+                    }
+                }
+            }
+            string[] tab = new string[1];
+            string koniec = tab[0];
+            if (wyjscie.Length < 2)
+            {
 
+            }
+            else
+            {
+                tab = wyjscie.Split(" ");
+
+                koniec = tab[0];
+                int poprzednik = int.Parse(tab[0]);
+                for (int i = 1; i < tab.Length - 1; i++)
+                {
+                    if (poprzednik != int.Parse(tab[i]))
+                    {
+                        koniec += $" {tab[i]}";
+                    }
+                    poprzednik = int.Parse(tab[i]);
+                }
+            }    if (wyjscie.Length > 0)
+                    Console.Write(koniec);
+                else
+                    Console.WriteLine("empty");
+            
         }
     }
 }
