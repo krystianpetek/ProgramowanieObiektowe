1fa9f529150affab3bc61990d3046caf8d815ff3
Krystian Petek <krystianpetek2@gmail.com>
2022-03-18 12:00:16 +0100

    updej

--- 16_tabliceProstokatne2D_2/Program.cs
+++ 16_tabliceProstokatne2D_2/Program.cs
@@ -6,7 +6,18 @@ namespace _16_tabliceProstokatne2D_2
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] wymiaryMacierzA = Console.ReadLine().Split(' '); 
+            int[] wartosciMacierzA = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), element => int.Parse(element));
+            string[] wymiaryMacierzB = Console.ReadLine().Split(' ');
+            int[] wartosciMacierzB = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), element => int.Parse(element));
+
+            int.TryParse(wymiaryMacierzA[0], out int nA);
+            int.TryParse(wymiaryMacierzA[1], out int mA);
+            
+            int.TryParse(wymiaryMacierzB[0], out int nB);
+            int.TryParse(wymiaryMacierzB[1], out int mB);
+        
+            
         }
     }
 }
