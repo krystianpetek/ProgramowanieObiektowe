d59022608c6d046cfdbf712939c6a6b89ab51697
Krystian Petek <krystianpetek2@gmail.com>
2022-03-20 23:51:36 +0100

    f

--- 20_sprawdzSudoku/Program.cs
+++ 20_sprawdzSudoku/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _20_sprawdzSudoku
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int[][] sudoku = new int[9][];
             for (int i = 0; i < sudoku.Length; i++)
@@ -12,19 +13,18 @@ namespace _20_sprawdzSudoku
                 sudoku[i] = Array.ConvertAll(Console.ReadLine().Split(" "), Convert);
             }
             if (!CheckLinear(sudoku))
-            { Console.WriteLine("no");
-                return;
-            }
-            int[][,] sudoku3x3 = new int[9][,];
-            if (!Check3x3(sudoku3x3))
             {
                 Console.WriteLine("no");
                 return;
             }
-            Console.WriteLine("yes");            
-        }
 
-        
+            if (!Check3x3(sudoku))
+            {
+                Console.WriteLine("no");
+                return;
+            }
+            Console.WriteLine("yes");
+        }
 
         public static int Convert(string x)
         {
@@ -36,15 +36,82 @@ namespace _20_sprawdzSudoku
             bool judge = true;
             for (int i = 0; i < sudoku.Length; i++)
             {
+                for (int j = 0; j < sudoku[i].Length; j++)
+                {
+                    for (int k = 0; k < sudoku[i].Length; k++)
+                    {
+                        if (j == k)
+                            continue;
 
+                        if (sudoku[i][j] == sudoku[i][k])
+                        {
+                            judge = false;
+                            return judge;
+                        }
+                    }
+                }
             }
             return judge;
         }
 
-        public static bool Check3x3(int[][,] sudoku3x3)
+        public static int[][,] Przepisanie(int[][] tab)
         {
-            throw new NotImplementedException();
+            int[][,] sudoku3x3 = new int[9][,];
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                sudoku3x3[i] = new int[3, 3];
+                for (int j = 0; j < tab[i].Length; j++)
+                {
+                    for (int k = 0; k < tab[i].Length; k++)
+
+                    {
+                        int X = 0;
+                        int Y = 0;
+                        try
+                        {
+                            X = j / 3;
+                            Y = k / 3;
+                        }
+                        catch (DivideByZeroException)
+                        {
+                        }
+                        sudoku3x3[i][j / 3, k / 3] = tab[j][k];
+                    }
+                }
+            }
+
+            return sudoku3x3;
         }
 
+        public static bool Check3x3(int[][] sudoku)
+        {
+            List<int[]> X = new List<int[]>();
+            bool judge = true;
+            int licznik = 0;
+            int[] tab = new int[3];
+
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+                for (int j = 0; j < sudoku[i].Length; j++)
+                {
+                    if (licznik == 3)
+                    {
+                        licznik = 0;
+                        X.Add(tab);
+                        tab = new int[3];
+                    }
+                    tab[licznik] = sudoku[i][j];
+                    licznik++;
+
+                    if (i == 8 && j == 8)
+                        X.Add(tab);
+                }
+            }
+
+
+
+            return judge;
+        }
     }
 }
---
+++ 21_odslonPlansze_Saper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _21_odslonPlansze_Saper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
