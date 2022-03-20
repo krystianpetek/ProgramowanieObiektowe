d1d2a398ce6029efafc7093f9333432cfb99e9d1
Krystian Petek <krystianpetek2@gmail.com>
2022-03-20 19:01:43 +0100

    kolejneZadanie

---
+++ 19_tablicaPostrzepiona/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _19_tablicaPostrzepiona
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            char[][] jagged = ReadJaggedArrayFromStdInput();
+            PrintJaggedArrayToStdOutput(jagged);
+            jagged = TransposeJaggedArray(jagged);
+            Console.WriteLine();
+            PrintJaggedArrayToStdOutput(jagged);
+        }
+
+        public static char[][] ReadJaggedArrayFromStdInput()
+        {
+            int.TryParse(Console.ReadLine(), out int numberOfRows);
+            char[][] jaggedArray = new char[numberOfRows][];
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                string[] inputStringArray = Console.ReadLine().Split(' ');
+                jaggedArray[i] = Array.ConvertAll<string, char>(inputStringArray, char.Parse);
+            }
+            return jaggedArray;
+        }
+
+        public static char[][] TransposeJaggedArray(char[][] tab)
+        {
+            int longestArray = 0;
+            foreach (char[] jagged in tab)
+            {
+                if (longestArray < jagged.Length)
+                    longestArray = jagged.Length;
+            }
+            char[][] newJaggedArray = new char[longestArray][];
+            for (int i = 0; i < longestArray; i++)
+            {
+                newJaggedArray[i] = new char[tab.Length];
+            }
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                for (int j = 0; j < tab[i].Length; j++)
+                {
+                    newJaggedArray[j][i] = tab[i][j];
+                }
+            }
+            return newJaggedArray;
+        }
+
+        public static void PrintJaggedArrayToStdOutput(char[][] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                for (int j = 0; j < tab[i].Length; j++)
+                {
+                    if (tab[i][j] == '\0')
+                    {
+                        
+                        Console.Write("  ");
+                    }
+                    else
+                    {
+                        Console.Write($"{tab[i][j]} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 20_sprawdzSudoku/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _20_sprawdzSudoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] sudoku = new int[9][];
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+                sudoku[i] = Array.ConvertAll(Console.ReadLine().Split(" "), Convert);
+            }
+            if (!CheckLinear(sudoku))
+            { Console.WriteLine("no");
+                return;
+            }
+            int[][,] sudoku3x3 = new int[9][,];
+            if (!Check3x3(sudoku3x3))
+            {
+                Console.WriteLine("no");
+                return;
+            }
+            Console.WriteLine("yes");            
+        }
+
+        
+
+        public static int Convert(string x)
+        {
+            return int.Parse(x);
+        }
+
+        public static bool CheckLinear(int[][] sudoku)
+        {
+            bool judge = true;
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+
+            }
+            return judge;
+        }
+
+        public static bool Check3x3(int[][,] sudoku3x3)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
