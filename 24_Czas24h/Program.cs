4d6819cf8e0981eadb7387d32d40ca431c8d1e66
Krystian Petek <krystianpetek2@gmail.com>
2022-03-21 23:51:34 +0100

    koniec na 21.03

--- 24_Czas24h/Program.cs
+++ 24_Czas24h/Program.cs
@@ -6,7 +6,39 @@ namespace _24_Czas24h
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Test 1
+            var t = new Czas24h(2, 15, 37);
+            t.Minuta = 20;
+            t.Godzina = 1;
+            t.Sekunda = 26;
+            Console.WriteLine(t);
+            // Test 2
+            t = new Czas24h(2, 15, 37);
+            t.Minuta = 20;
+            t.Godzina = 24;
+            Console.WriteLine(t);
+            // Test 3
+            t = new Czas24h(2, 15, 37);
+            t.Minuta = -20;
+            t.Godzina = 23;
+            Console.WriteLine(t);
+            // Test 4
+            t = new Czas24h(2, 15, 37);
+            t.Minuta = 20;
+            t.Godzina = 23;
+            t.Godzina = 1;
+            t.Sekunda = 15;
+            t.Minuta = 10;
+            t.Sekunda = 23;
+            t.Sekunda = 1;
+            t.Minuta = 12;
+            Console.WriteLine(t);
+            // Test 5
+            t = new Czas24h(24, 15, 37);
+            t.Minuta = 20;
+            t.Godzina = 23;
+            t.Godzina = 1;
+            Console.WriteLine(t);
         }
     }
 
@@ -17,26 +49,40 @@ namespace _24_Czas24h
         public int Sekunda
         {
             get => liczbaSekund - Godzina * 60 * 60 - Minuta * 60;
-            set => liczbaSekund = value;
+            set
+            {
+                if (value < 0 || value > 59)
+                    throw new ArgumentException("error");
+                liczbaSekund = value + Godzina * 60 * 60 + Minuta * 60;
+            }
         }
 
         public int Minuta
         {
             get => (liczbaSekund / 60) % 60;
-            // uzupełnij kod - zdefiniuj setters'a
+            set
+            {
+                if (value < 0 || value > 59)
+                    throw new ArgumentException("error");
+                liczbaSekund = Sekunda + Godzina * 60 * 60 + value * 60;
+            }
         }
 
         public int Godzina
         {
             get => liczbaSekund / 3600;
-            // uzupełnij kod - zdefiniuj setters'a
+            set
+            {
+                if (value < 0 || value > 23)
+                    throw new ArgumentException("error");
+                liczbaSekund = Sekunda + value * 60 * 60 + Minuta * 60;
+            }
         }
 
         public Czas24h(int godzina, int minuta, int sekunda)
         {
-            // uzupełnij kod zgłaszając wyjątek ArgumentException
-            // w sytuacji niepoprawnych danych
-
+            if (godzina > 23 || godzina < 0 || minuta > 59 || minuta < 0 || sekunda > 59 || sekunda < 0)
+                throw new ArgumentException("error");
             liczbaSekund = sekunda + 60 * minuta + 3600 * godzina;
         }
 
--- 25_kolekcjeAgregatorLogow/Program.cs
+++ 25_kolekcjeAgregatorLogow/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _25_kolekcjeAgregatorLogow
 {
     internal class Program
     {
+//7
+//192.168.0.11 peter 33
+//10.10.17.33 alex 12
+//10.10.17.35 peter 30
+//10.10.17.34 peter 120
+//10.10.17.34 peter 120
+//212.50.118.81 alex 46
+//212.50.118.81 alex 4
+
+//2
+//84.238.140.178 mol 25
+//84.238.140.178 mol 35
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int.TryParse(Console.ReadLine(), out int howManyLogs);
+            //string[][] lista = new string[howManyLogs][];
+            //for(int i = 0; i < howManyLogs; i++)
+            //{
+            //    lista[i] = Console.ReadLine().Split(" ");
+            //}
+            List<Logi> listaLogow = new List<Logi>();
+            for (int i = 0; i < howManyLogs; i++)
+            {
+                string[] linia = Console.ReadLine().Split(" ");
+                Logi x = new Logi(linia[0], linia[1], int.Parse(linia[2]));
+                listaLogow.Add(x);
+            }
         }
+    }
+    public class Logi
+    {
+        public Logi(string ip, string nazwa, int czas)
+        {
+            Nazwa = nazwa;
+            IP = ip;
+            Czas = czas;
+        }
+        public string Nazwa { get; set; }
+        public string IP { get; set; }
+        public int Czas { get; set; }
+        public int aIP => int.Parse(IP.Split(".")[0]);
+        public int bIP => int.Parse(IP.Split(".")[1]);
+        public int cIP => int.Parse(IP.Split(".")[2]);
+        public int dIP => int.Parse(IP.Split(".")[3]);
+
     }
 }
