aa33eaa73f49099968f9ae6b95236f147903e178
Krystian Petek <krystianpetek2@gmail.com>
2022-03-21 16:10:48 +0100

    duzy update

--- 20_sprawdzSudoku/Program.cs
+++ 20_sprawdzSudoku/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace _20_sprawdzSudoku
 {
+    // dopracować
     internal class Program
     {
         private static void Main(string[] args)
@@ -12,7 +13,12 @@ namespace _20_sprawdzSudoku
             {
                 sudoku[i] = Array.ConvertAll(Console.ReadLine().Split(" "), Convert);
             }
-            if (!CheckLinear(sudoku))
+            if (!CheckWidth(sudoku))
+            {
+                Console.WriteLine("no");
+                return;
+            }
+            if (!CheckHeight(sudoku))
             {
                 Console.WriteLine("no");
                 return;
@@ -26,12 +32,35 @@ namespace _20_sprawdzSudoku
             Console.WriteLine("yes");
         }
 
+        private static bool CheckHeight(int[][] sudoku)
+        {
+            bool judge = true;
+            for (int i = 0; i < sudoku.Length; i++)
+            {
+                for (int j = 0; j < sudoku[i].Length; j++)
+                {
+                    for (int k = 0; k < sudoku[i].Length; k++)
+                    {
+                        if (j == k)
+                            continue;
+
+                        if (sudoku[j][i] == sudoku[k][i])
+                        {
+                            judge = false;
+                            return judge;
+                        }
+                    }
+                }
+            }
+            return judge;
+        }
+
         public static int Convert(string x)
         {
             return int.Parse(x);
         }
 
-        public static bool CheckLinear(int[][] sudoku)
+        public static bool CheckWidth(int[][] sudoku)
         {
             bool judge = true;
             for (int i = 0; i < sudoku.Length; i++)
@@ -54,64 +83,103 @@ namespace _20_sprawdzSudoku
             return judge;
         }
 
-        public static int[][,] Przepisanie(int[][] tab)
-        {
-            int[][,] sudoku3x3 = new int[9][,];
+        //public static int[][,] Przepisanie(int[][] tab)
+        //{
+        //    int[][,] sudoku3x3 = new int[9][,];
 
-            for (int i = 0; i < tab.Length; i++)
-            {
-                sudoku3x3[i] = new int[3, 3];
-                for (int j = 0; j < tab[i].Length; j++)
-                {
-                    for (int k = 0; k < tab[i].Length; k++)
+        //    for (int i = 0; i < tab.Length; i++)
+        //    {
+        //        sudoku3x3[i] = new int[3, 3];
+        //        for (int j = 0; j < tab[i].Length; j++)
+        //        {
+        //            for (int k = 0; k < tab[i].Length; k++)
 
-                    {
-                        int X = 0;
-                        int Y = 0;
-                        try
-                        {
-                            X = j / 3;
-                            Y = k / 3;
-                        }
-                        catch (DivideByZeroException)
-                        {
-                        }
-                        sudoku3x3[i][j / 3, k / 3] = tab[j][k];
-                    }
-                }
-            }
+        //            {
+        //                int X = 0;
+        //                int Y = 0;
+        //                try
+        //                {
+        //                    X = j / 3;
+        //                    Y = k / 3;
+        //                }
+        //                catch (DivideByZeroException)
+        //                {
+        //                }
+        //                sudoku3x3[i][j / 3, k / 3] = tab[j][k];
+        //            }
+        //        }
+        //    }
 
-            return sudoku3x3;
-        }
+        //    return sudoku3x3;
+        //}
 
         public static bool Check3x3(int[][] sudoku)
         {
-            List<int[]> X = new List<int[]>();
             bool judge = true;
-            int licznik = 0;
-            int[] tab = new int[3];
+            int[] lista1 = new int[9];
+            int[] lista2 = new int[9];
+            int[] lista3 = new int[9];
+
+            int x = 0;
+            int mnoznik = 0;
 
             for (int i = 0; i < sudoku.Length; i++)
             {
+                if (i == 3)
+                {
+                    if (!jeszczeJedenCheck(lista1))
+                        return false;
+                    if (!jeszczeJedenCheck(lista2))
+                        return false;
+                    if (!jeszczeJedenCheck(lista3))
+                        return false;
+                    lista1 = new int[9];
+                    lista2 = new int[9];
+                    lista3 = new int[9];
+                    mnoznik = 0;
+                }
+                if (i == 0 || i == 3 || i == 6)
+                {
+                    mnoznik = 0;
+                }
+                else
+                {
+                    mnoznik += 3;
+                }
                 for (int j = 0; j < sudoku[i].Length; j++)
                 {
-                    if (licznik == 3)
-                    {
-                        licznik = 0;
-                        X.Add(tab);
-                        tab = new int[3];
-                    }
-                    tab[licznik] = sudoku[i][j];
-                    licznik++;
+                    
+                    if (j < 3)
+                        lista1[x + mnoznik] = sudoku[i][j];
+                    else if (j < 6)
+                        lista2[x + mnoznik] = sudoku[i][j];
+                    else if (j < 9)
+                        lista3[x + mnoznik] = sudoku[i][j];
 
-                    if (i == 8 && j == 8)
-                        X.Add(tab);
+                    x++;
+                    if (x % 3 == 0)
+                    {
+                        x = 0;
+                    }
+                }
+                
+            }
+            return judge;
+        }
+        public static bool jeszczeJedenCheck(int[] tab)
+        {
+            for (int c = 0; c < tab.Length; c++)
+            {
+                for (int d = 0; d < tab.Length; d++)
+                {
+                    if (c == d)
+                        continue;
+
+                    if (tab[c] == tab[d])
+                        return false;
                 }
             }
-
-
-
-            return judge;
+            return true;
         }
     }
 }
--- 21_odslonPlansze_Saper/Program.cs
+++ 21_odslonPlansze_Saper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _21_odslonPlansze_Saper
 {
@@ -6,7 +7,164 @@ namespace _21_odslonPlansze_Saper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] wymiary = Console.ReadLine().Split(' ');
+            int.TryParse(wymiary[0], out int x);
+            int.TryParse(wymiary[1], out int y);
+            List<string[]> plansza = new List<string[]>();
+            for (int i = 0; i < x; i++)
+            {
+                string linia = Console.ReadLine();
+                string[] wiersz = new string[linia.Length];
+                int iter = 0;
+                foreach (char znak in linia)
+                {
+                    wiersz[iter] = znak.ToString();
+                    iter++;
+                }
+                plansza.Add(wiersz);
+            }
+
+            for (int i = 0; i < plansza.Count; i++)
+            {
+                for (int j = 0; j < plansza[i].Length; j++)
+                {
+                    plansza[i][j] = DoIt(i, j);
+                }
+            }
+
+            string DoIt(int i, int j)
+            {
+                if (plansza[i][j] == "*")
+                    return "*";
+
+                if (plansza[i][j] == ".")
+                {
+                    int licznik = 0;
+                    if (i == 0 && j == 0)
+                    {
+                        licznik += liczenie(Kierunek.Prawo, i, j);
+                        licznik += liczenie(Kierunek.DolPrawo, i, j);
+                        licznik += liczenie(Kierunek.Dol, i, j);
+                        // prawo, prawo dol, dol
+                    }
+                    else if (i == 0 && j == plansza[i].Length - 1)
+                    {
+                        licznik += liczenie(Kierunek.Dol, i, j);
+                        licznik += liczenie(Kierunek.DolLewo, i, j);
+                        licznik += liczenie(Kierunek.Lewo, i, j);
+                        // dol, lewo dol, lewo
+                    }
+                    else if (i == 0 && j > 0)
+                    {
+                        licznik += liczenie(Kierunek.Prawo, i, j);
+                        licznik += liczenie(Kierunek.DolPrawo, i, j);
+                        licznik += liczenie(Kierunek.Dol, i, j);
+                        licznik += liczenie(Kierunek.DolLewo, i, j);
+                        licznik += liczenie(Kierunek.Lewo, i, j);
+                        // prawo, prawo dol, dol, lewo dol, lewo
+                    }
+                    
+                    else if (i > 0 && j == plansza[i].Length - 1)
+                    {
+                        licznik += liczenie(Kierunek.Dol, i, j);
+                        licznik += liczenie(Kierunek.DolLewo, i, j);
+                        licznik += liczenie(Kierunek.Lewo, i, j);
+                        licznik += liczenie(Kierunek.GoraLewo, i, j);
+                        licznik += liczenie(Kierunek.Gora, i, j);
+                        // dol, lewo dol, lewo, lewo gora, gora
+                    }
+                    else if (i == plansza.Count - 1 && j == plansza[i].Length - 1)
+                    {
+                        licznik += liczenie(Kierunek.Lewo, i, j);
+                        licznik += liczenie(Kierunek.GoraLewo, i, j);
+                        licznik += liczenie(Kierunek.Gora, i, j);
+                        // lewo, lewo gora, gora
+                    }
+                    else if (i == plansza.Count - 1 && j > 0)
+                    {
+                        licznik += liczenie(Kierunek.Lewo, i, j);
+                        licznik += liczenie(Kierunek.GoraLewo, i, j);
+                        licznik += liczenie(Kierunek.Gora, i, j);
+                        licznik += liczenie(Kierunek.GoraPrawo, i, j);
+                        licznik += liczenie(Kierunek.Prawo, i, j);
+                        // lewo, lewo gora, gora, prawo gora, prawo
+                    }
+                    else if (i == plansza.Count - 1 && j == 0)
+                    {
+                        licznik += liczenie(Kierunek.Gora, i, j);
+                        licznik += liczenie(Kierunek.GoraPrawo, i, j);
+                        licznik += liczenie(Kierunek.Prawo, i, j);
+                        // gora, prawo gora, prawo
+                    }
+                    else if (i > 0 && j == 0)
+                    {
+
+                        licznik += liczenie(Kierunek.Gora, i, j);
+                        licznik += liczenie(Kierunek.GoraPrawo, i, j);
+                        licznik += liczenie(Kierunek.Prawo, i, j);
+                        licznik += liczenie(Kierunek.DolPrawo, i, j);
+                        licznik += liczenie(Kierunek.Dol, i, j);
+                        // gora, prawo gora, prawo, prawo dol, dol
+                    }
+                    if (licznik == 0)
+                        return ".";
+                    else
+                        return $"{licznik}";
+                }
+
+                return plansza[i][j];
+            }
+            int liczenie(Kierunek kierunek, int x, int y)
+            {
+                switch(kierunek)
+                {
+                    case Kierunek.Gora:
+                        if (plansza[x-1][y] == "*")
+                            return 1;
+                        break;
+                    case Kierunek.GoraPrawo:
+                        if (plansza[x-1][y+1] == "*")
+                            return 1;
+                        break;
+                    case Kierunek.Prawo:
+                        if (plansza[x][y+1] == "*")
+                            return 1;
+                        break;
+                    case Kierunek.DolPrawo:
+                        if (plansza[x+1][y+1] == "*")
+                            return 1;
+                        break;
+                    case Kierunek.Dol:
+                        if (plansza[x+1][y] == "*")
+                            return 1;
+                        break;
+                    case Kierunek.DolLewo:
+                        if (plansza[x+1][y-1] == "*")
+                            return 1;
+                        break;
+                    case Kierunek.Lewo:
+                        if (plansza[x][y-1] == "*")
+                            return 1;
+                        break;
+                    case Kierunek.GoraLewo:
+                        if (plansza[x-1][y-1] == "*")
+                            return 1;
+                        break;
+                }
+                return 0;
+            }
         }
+    }
+    public enum Kierunek
+    {
+
+        GoraLewo = 7,
+        Gora = 8,
+        GoraPrawo = 9,
+        Prawo = 6,
+        DolPrawo = 3,
+        Dol = 2,
+        DolLewo = 1,
+        Lewo = 4
     }
 }
