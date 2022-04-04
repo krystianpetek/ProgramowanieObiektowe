bf81f82ff947d101a5c87d08dcffae5fadc7bc78
Krystian Petek <krystianpetek2@gmail.com>
2022-04-04 21:42:44 +0200

    koniec metody_rozszerzające

--- Metody_Rozszerzajace/Program.cs
+++ Metody_Rozszerzajace/Program.cs
@@ -3,9 +3,39 @@
 string napis = "Ala ma kota, as to Ali pies";
 Console.WriteLine(Utility.WordCount(napis));
 
-Console.WriteLine(Utility.WordCount(napis,' ','\t',',','.','!','?','`'));
+Console.WriteLine(Utility.WordCount(napis, ' ', '\t', ',', '.', '!', '?', '`'));
 
 Console.WriteLine(napis.WordCountExtended());
 
-Console.WriteLine(napis.WordCountExtended( delimiters: new char[] {' ',',','\t',',','.','!','?'} ));
+Console.WriteLine(napis.WordCountExtended(delimiters: new char[] { ' ', ',', '\t', ',', '.', '!', '?' }));
 Console.WriteLine(napis.WordCountExtended(' ', ',', '\t', ',', '.', '!', '?'));
+
+// zadanie 1
+Console.WriteLine(napis.BezSamoglosek());
+
+// zadanie 2
+napis = "432";
+Console.WriteLine(napis.IsNumeric());
+
+// zadanie 3
+var lista = new List<int> { 0, 1, 2, 3, 4 };
+Console.WriteLine(lista.Dump());
+
+// zadanie 4
+var numDividedBy2 = from item in lista where item % 2 == 0 select item;
+numDividedBy2.PrintLn();
+
+// zadanie 5
+int[] mediana = new int[] { 0, 1, 2, 2, 2, 2, 2, 3, 4, 6, 7, 8, 9, 9, 9, 10 };
+Console.WriteLine(mediana.Mediana());
+
+// zadanie 6
+int liczba = 5;
+Console.WriteLine(liczba.Between<int>(0, 9));
+Console.WriteLine(liczba.Between<int>(0, 5));
+Console.WriteLine(liczba.Between<int>(0, 4));
+napis = "alicja";
+Console.WriteLine(napis.Between("ala","baba"));
+
+// zadanie 7
+Console.WriteLine(liczba.BetterBetween<int>(0, 9, (x, y) => x.CompareTo(y)));
--- Metody_Rozszerzajace/Utility.cs
+++ Metody_Rozszerzajace/Utility.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Metody_Rozszerzajace
+﻿namespace Metody_Rozszerzajace
 {
     public static class Utility
     {
@@ -12,7 +6,7 @@ namespace Metody_Rozszerzajace
         {
             return napis.Split(" ", StringSplitOptions.RemoveEmptyEntries).Length;
         }
-        
+
         public static int WordCount(string napis, params char[] delimiters)
         {
             return napis.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
@@ -28,10 +22,78 @@ namespace Metody_Rozszerzajace
             return napis.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
         }
 
+        // zadanie 1
         public static string BezSamoglosek(this string napis)
         {
-
+            string[] tab = napis.Split('a', 'e', 'i', 'o', 'u', 'y', 'ą', 'ę');
+            string wynik = string.Empty;
+            foreach (var x in tab)
+                wynik += x;
+            return wynik;
         }
 
+        // zadanie 2
+        public static bool IsNumeric(this string napis)
+        {
+            return int.TryParse(napis, out int result);
+        }
+
+        // zadanie 3
+        public static string Dump(this List<int> lista)
+        {
+            string result = "{";
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (i == lista.Count - 1)
+                    result += $"{lista[i]}" + "}";
+                else
+                {
+                    result += $"{lista[i]}, ";
+                }
+            }
+            return result;
+        }
+
+        // zadanie 4
+        public static void PrintLn<T>(this IEnumerable<T> enumerator)
+        {
+            foreach (var x in enumerator)
+                Console.WriteLine(x);
+        }
+
+        // zadanie 5 - mediana
+        public static double Mediana(this IEnumerable<int> listaPosortowana)
+        {
+            var x = listaPosortowana.ToArray();
+            var query = listaPosortowana.Count() / 2;
+            if (listaPosortowana.Count() % 2 == 0)
+            {
+                var jeden = x[query - 1];
+                var dwa = x[query];
+                return jeden + dwa / 2;
+            }
+            return x[query];
+        }
+
+        // zadanie 6
+        public static bool Between<T>(this T mid, T lower, T upper) where T : IComparable<T>
+        {
+            var wynik1 = mid.CompareTo(lower);
+            var wynik2 = upper.CompareTo(mid);
+
+            if (wynik1 == -1 || wynik2 == -1)
+                return false;
+            return true;
+        }
+
+        // zadanie 7
+        public static bool BetterBetween<T>(this T mid, T lower, T upper, Comparison<T> comparison)
+        {
+            var wynik1 = comparison.Invoke(mid, lower);
+            var wynik2 = comparison.Invoke(upper, mid);
+            if (wynik1 == -1 || wynik2 == -1)
+                return false;
+            return true;
+        }
     }
 }
