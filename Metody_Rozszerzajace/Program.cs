34dd576ae95a1b8b8089356f6ed56344e5d03f9a
Krystian Petek <krystianpetek2@gmail.com>
2022-04-04 16:20:50 +0200

    z

--- Metody_Rozszerzajace/Program.cs
+++ Metody_Rozszerzajace/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Metody_Rozszerzajace;
+
+string napis = "Ala ma kota, as to Ali pies";
+Console.WriteLine(Utility.WordCount(napis));
+
+Console.WriteLine(Utility.WordCount(napis,' ','\t',',','.','!','?','`'));
+
+Console.WriteLine(napis.WordCountExtended());
+
+Console.WriteLine(napis.WordCountExtended( delimiters: new char[] {' ',',','\t',',','.','!','?'} ));
+Console.WriteLine(napis.WordCountExtended(' ', ',', '\t', ',', '.', '!', '?'));
---
+++ Metody_Rozszerzajace/Utility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody_Rozszerzajace
+{
+    public static class Utility
+    {
+        public static int WordCount(string napis)
+        {
+            return napis.Split(" ", StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        
+        public static int WordCount(string napis, params char[] delimiters)
+        {
+            return napis.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static int WordCountExtended(this string napis)
+        {
+            return napis.Split(" ", StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static int WordCountExtended(this string napis, params char[] delimiters)
+        {
+            return napis.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string BezSamoglosek(this string napis)
+        {
+
+        }
+
+    }
+}
