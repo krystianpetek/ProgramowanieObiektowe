77c5fc50e5ec786d3a488ae61bf22ef020487369
Krystian Petek <krystianpetek2@gmail.com>
2022-04-05 15:43:55 +0200

    update

--- ImplementacjaInterfejsow/Pracownik.cs
+++ ImplementacjaInterfejsow/Pracownik.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace ImplementacjaInterfejsow
 {
-    internal class Pracownik
+    internal class Pracownik : IEquatable<Pracownik>, IComparable<Pracownik>
     {
         private string nazwisko;
         private DateTime dataZatrudnienia;
         private decimal wynagrodzenie;
+
+        public Pracownik(string nazwisko, DateTime dataZatrudnienia, decimal wynagrodzenie)
+        {
+            Nazwisko = nazwisko;
+            DataZatrudnienia = dataZatrudnienia;
+            Wynagrodzenie = wynagrodzenie;
+        }
+        public Pracownik() : this("Anonim", DateTime.Now, 0) { }
+
         public string Nazwisko
         {
             get {
@@ -32,12 +41,99 @@ namespace ImplementacjaInterfejsow
         }
         public decimal Wynagrodzenie { get { return wynagrodzenie; }
             set {
-                if (value < 0) wynagrodzenie = 0;
+
+                if (value < 0) { wynagrodzenie = 0;
+                    return;
+                }
+                wynagrodzenie = value;
             } 
         }
+        public double CzasZatrudnienia => Math.Floor((DateTime.Now - DataZatrudnienia).TotalDays/30); 
+        
         public override string ToString()
         {
-            return $"({Nazwisko}, {DataZatrudnienia}, {Wynagrodzenie})}";
+            return $"({Nazwisko}, {DataZatrudnienia.ToShortDateString()} ({CzasZatrudnienia}), {Wynagrodzenie})";
+        }
+
+        public bool Equals(Pracownik? other)
+        {
+            if (other.Nazwisko != this.Nazwisko)
+                return false;
+            if (other.DataZatrudnienia != this.DataZatrudnienia)
+                return false;
+            if (other.Wynagrodzenie != this.Wynagrodzenie)
+                return false;
+
+            return true;
+        }
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Pracownik);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Nazwisko, this.DataZatrudnienia, this.Wynagrodzenie);
+        }
+        public static bool Equals(Pracownik p1, Pracownik p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public int CompareTo(Pracownik? other)
+        {
+            int zmienna = 0, wynik = 0;
+
+            zmienna = this.Wynagrodzenie.CompareTo(other.Wynagrodzenie);
+            if (zmienna > 0)
+                wynik = 1;
+            if (zmienna < 0)
+                wynik = -1;
+
+            zmienna = this.DataZatrudnienia.CompareTo(other.DataZatrudnienia);
+            if (zmienna > 0)
+                wynik = 1;
+            if (zmienna < 0)
+                wynik = -1;
+
+            zmienna = this.Nazwisko.CompareTo(other.Nazwisko);
+            if (zmienna > 0)
+                wynik = 1;
+            if (zmienna < 0)
+                wynik = -1;
+
+            return wynik;
+        }
+
+        //public int CompareTo(Pracownik other)
+        //{
+        //    if (other is null) return 1; // w C#: null jest najmniejszą wartością (`this > null`)
+        //    if (this.Equals(other)) return 0; //zgodność z Equals (`this == other`)
+
+        //    if (this.Nazwisko != other.Nazwisko)
+        //        return this.Nazwisko.CompareTo(other.Nazwisko);
+
+        //    // ponieważ nazwiska równe, porządkujemy wg daty
+        //    if (!this.DataZatrudnienia.Equals(other.DataZatrudnienia)) // != zamiast !Equals
+        //        return this.DataZatrudnienia.CompareTo(other.DataZatrudnienia);
+
+        //    // ponieważ nazwiska równe i daty równe, porządkujemy wg wynagrodzenia
+        //    return this.Wynagrodzenie.CompareTo(other.Wynagrodzenie);
+        //}
+
+
+        public static bool operator == (Pracownik p1, Pracownik p2)
+        {
+            if (p1.Nazwisko == p2.Nazwisko &&
+                p1.DataZatrudnienia == p2.DataZatrudnienia &&
+                p1.Wynagrodzenie == p2.Wynagrodzenie)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Pracownik p1, Pracownik p2)
+        {
+            if (p1 == p2)
+                return false;
+            return true;
         }
     }
     
--- ImplementacjaInterfejsow/Program.cs
+++ ImplementacjaInterfejsow/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ImplementacjaInterfejsow;
+
+// zadanie 1
+Pracownik pracownik = new Pracownik("Petek", new DateTime(2019, 08, 01),2800);
+Console.WriteLine(pracownik.CzasZatrudnienia);
+Console.WriteLine(pracownik.ToString());
+Pracownik pracownik2 = new Pracownik("Petek", new DateTime(2019, 08, 01), 2800);
+Console.WriteLine($"{"Equals:",-10}{pracownik.Equals(pracownik2)}");
+Console.WriteLine($"{"==:",-10}{pracownik == pracownik2}");
+Console.WriteLine($"{"!=:",-10}{pracownik != pracownik2}");
+Console.WriteLine($"{"HashCode p1: ",-15}{pracownik.GetHashCode()}");
+Console.WriteLine($"{"HashCode p2: ",-15}{pracownik2.GetHashCode()}");
+Console.WriteLine($"{"Equals Hash: ",-15}{pracownik.GetHashCode() == pracownik2.GetHashCode()}");
+Console.WriteLine(Pracownik.Equals(pracownik, pracownik2));
+
+// zadanie 2
+Pracownik pracownik01 = new Pracownik("Warchał", new DateTime(2020, 01, 05), 3000);
+Pracownik pracownik02 = new Pracownik("Warchał", new DateTime(2018, 10, 02), 3100);
+Pracownik pracownik03 = new Pracownik("Petek", new DateTime(2020, 01, 05), 3200);
+Pracownik pracownik04 = new Pracownik("Merek", new DateTime(2012, 12, 12), 2300);
+Pracownik pracownik05 = new Pracownik("Porębski", new DateTime(2014, 02, 04), 3200);
+List<Pracownik> listaPracownikow = new List<Pracownik>()
+{
+    pracownik01,
+    pracownik02,
+    pracownik03,
+    pracownik04,
+    pracownik05
+};
+foreach(Pracownik employee in listaPracownikow)
+    Console.WriteLine(employee);
+
+listaPracownikow.Sort();
+Console.WriteLine();
+foreach (Pracownik employee in listaPracownikow)
+    Console.WriteLine(employee);
